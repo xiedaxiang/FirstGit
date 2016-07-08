6b9ef32808df37861aa8c13bccffd591c44cc7f6
xiedaxiang <bubaizhao@126.com>
2016-07-08 18:58:16 +0800

    update 03

--- src/com/atgugui/git/FirstGitTest.java
+++ src/com/atgugui/git/FirstGitTest.java
@@ -5,6 +5,7 @@ public class FirstGitTest {
 	public static void main(String[] args) {
 		System.out.println("My first...");
 		System.out.println("My Two...");
+		System.out.println("My three");
 	}
 
 }
